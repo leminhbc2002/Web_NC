664a76cc840a064d9cd2a537990c7df808951d08
leminhbc2002 <leminh.bc2002@gmail.com>
2023-07-23 00:12:07 +0700

    first commit

---
+++ server/models/OrderDAO.js
@@ -0,0 +1,12 @@
+require('../utils/MongooseUtil');
+const Models = require('./Models');
+
+const OrderDAO = {
+  async insert(order) {
+    const mongoose = require('mongoose');
+    order._id = new mongoose.Types.ObjectId();
+    const result = await Models.Order.create(order);
+    return result;
+  }
+};
+module.exports = OrderDAO;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,9 @@
+const MyConstants = {
+    DB_SERVER: 'cluster15.ctrqflj.mongodb.net',
+    DB_USER: 'leminhbc2002',
+    DB_PASS: 'mile15102002',
+    DB_DATABASE:'Shopping-Yame',
+    JWT_SECRET: 'mile',
+    JWT_EXPIRES: '600000' // in milliseconds
+  };
+  module.exports = MyConstants;
